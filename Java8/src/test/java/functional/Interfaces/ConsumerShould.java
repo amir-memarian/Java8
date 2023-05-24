24b58a4050a470df95daae44af6428524fca0786
amir-memarian <memarian.email@gmail.com>
2023-05-24 13:45:51 +0330

    finish

--- Java8/src/test/java/functional/Interfaces/ConsumerShould.java
+++ Java8/src/test/java/functional/Interfaces/ConsumerShould.java
@@ -1,17 +1,11 @@
 package functional.Interfaces;
 
-import com.sun.deploy.net.MessageHeader;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Consumer;
-import java.util.function.DoubleConsumer;
-import java.util.function.IntConsumer;
-import java.util.function.LongConsumer;
 
-import static java.lang.System.arraycopy;
 import static java.lang.System.out;
 import static org.assertj.core.api.Assertions.assertThat;
 
--- Java8/src/test/java/optional/OptionalShould.java
+++ Java8/src/test/java/optional/OptionalShould.java
@@ -64,6 +64,7 @@ public class OptionalShould {
 
     }
 
+
     @Test
     void have_default_values() {
         String name = "Amir";
